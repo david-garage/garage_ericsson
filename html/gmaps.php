a9365cfca5fc8918cf9399583d82d86e1625abd2
david-garage <36634472+david-garage@users.noreply.github.com>
2018-05-18 16:32:14 +0200

    accueil.html modif sur gmaps avec selectbox

--- html/gmaps.php
+++ html/gmaps.php
@@ -8,26 +8,33 @@
       #map {
         height: 100%;
 
+
       }
       /* Optional: Makes the sample page fill the window. */
       html, body {
         height: 100%;
         margin: 00px;
-        padding: 100px;
+        padding: 00px;
       }
     </style>
 <body>
-  <div class="container">
-   <div class="dropDownControl" id="ddControl" title="A custom drop down select with mixed elements" onclick="(document.getElementById('myddOptsDiv').style.display == 'block') ? document.getElementById('myddOptsDiv').style.display = 'none' : document.getElementById('myddOptsDiv').style.display = 'block';"">
-    My Box
+  <div id="container">
+   <div class="dropDownControl" id="ddControl" title="Selection affichage gaz" onclick="(document.getElementById('myddOptsDiv').style.display == 'block') ? document.getElementById('myddOptsDiv').style.display = 'none' : document.getElementById('myddOptsDiv').style.display = 'block';"">
+    Selection Gaz
     <img class="dropDownArrow" src="http://maps.gstatic.com/mapfiles/arrow-down.png" />
    </div>
    <div class = "dropDownOptionsDiv" id="myddOptsDiv">
-    <div class = "dropDownItemDiv" id="mapOpt"  title="This acts like a button or click event" onClick="alert('dav1')">
-     Option 1
+    <div class = "dropDownItemDiv" id="mapOpt"  title="CO" onClick="alert('CO séléctionné')">
+     CO
     </div>
-    <div class = "dropDownItemDiv" id="satelliteOpt" title="This acts like a button or click event" onClick="alert('option2')">
-     Option 2
+    <div class = "dropDownItemDiv" id="satelliteOpt" title="CO2" onClick="alert('CO2 selectionné')">
+     CO2
+    </div>
+    <div class = "dropDownItemDiv" id="NO2Opt" title="NO2" onClick="alert('NO2 selectionné')">
+     NO2
+    </div>
+    <div class = "dropDownItemDiv" id="APOpt" title="AP" onClick="alert('AP selectionné')">
+     AP
     </div>
     <div class="separatorDiv"></div>
     <div class="checkboxContainer" title="This allows for multiple selection/toggling on/off" onclick="(document.getElementById('terrainCheck').style.display == 'block') ? document.getElementById('terrainCheck').style.display = 'none' : document.getElementById('terrainCheck').style.display = 'block';">
@@ -54,6 +61,38 @@ ini_set('display_errors', 1);
       var map;
       var ericsson  = {lat: 48.726772  , lng: 2.263562};
 
+      function CenterControl(controlDiv, map) {
+
+              // Set CSS for the control border.
+              var controlUI = document.createElement('div');
+              controlUI.style.backgroundColor = '#fff';
+              controlUI.style.border = '2px solid #fff';
+              controlUI.style.borderRadius = '3px';
+              controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+              controlUI.style.cursor = 'pointer';
+              controlUI.style.marginBottom = '22px';
+              controlUI.style.textAlign = 'center';
+              controlUI.title = 'Click to recenter the map';
+              controlDiv.appendChild(controlUI);
+
+              // Set CSS for the control interior.
+              var controlText = document.createElement('div');
+              controlText.style.color = 'rgb(25,25,25)';
+              controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+              controlText.style.fontSize = '16px';
+              controlText.style.lineHeight = '38px';
+              controlText.style.paddingLeft = '5px';
+              controlText.style.paddingRight = '5px';
+              controlText.innerHTML = 'Center Map';
+              controlUI.appendChild(controlText);
+
+              // Setup the click event listeners: simply set the map to Chicago.
+              controlUI.addEventListener('click', function() {
+                map.setCenter(chicago);
+              });
+
+            }
+
 
 
 
@@ -65,6 +104,18 @@ ini_set('display_errors', 1);
 	zoomControl : true,
 	scaleControl : true
         });
+        // Create the DIV to hold the control and call the CenterControl()
+                // constructor passing in this DIV.
+                var centerControlDiv = document.getElementById('container');
+                var map_obj = document.getElementById('map');
+				map_obj.appendChild('centerControlDiv');
+               // document.body.appendChild('centerControlDiv');
+                var centerControl = new CenterControl(centerControlDiv, map);
+
+                centerControlDiv.index = 1;
+                map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+
+
 
 
 for (var i = 0; i < planelatlong.length; i++) {
@@ -97,15 +148,9 @@ for (var i = 0; i < planelatlong.length; i++) {
                 center: {lat: point[0], lng: point[1]},
                 radius:  30
               });
-   }
+}
 
-
-
-
-
-
-             }
-
+}
 
 
     </script>
